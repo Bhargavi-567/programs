6eb4c2244436487eb8f147258fcac8525b1d8f4e
bhargavi_567 <bhargavi.madarapu22@gmail.com>
2022-01-31 21:01:40 +0530

    program added

---
+++ using System;.cs
@@ -0,0 +1,28 @@
+using System;
+
+ 
+
+
+namespace Demobhagi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a=Convert.ToInt32(Console.ReadLine());
+            if(a%400==0)
+            {
+                Console.WriteLine("leap year");
+
+            }
+            else if (a%100==0)
+            Console.WriteLine("not leap year");
+            else if(a%4==0)
+            {
+Console.WriteLine("leap year");
+            }
+            
+        }
+    }
+}
