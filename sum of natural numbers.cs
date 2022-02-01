c64ffcd1577780a36800bae4aab2ffd429dc2840
bhargavi_567 <bhargavi.madarapu22@gmail.com>
2022-02-01 10:35:15 +0530

    natural numbers

---
+++ sum of natural numbers.cs
@@ -0,0 +1,24 @@
+using System;
+
+
+
+namespace Demobhagi
+ class Program
+{
+     static void Main(string[] args)
+    {
+        int i, n, sum = 0;
+
+        
+
+        Console.Write("Input number of terms : ");
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\nThe first {0} natural number are :\n", n);
+        for (i = 1; i <= n; i++)
+        {
+            Console.Write("{0} ", i);
+            sum += i;
+        }
+        Console.Write("\nThe Sum of Natural Number upto {0} terms : {1} \n", n, sum);
+    }
+}
